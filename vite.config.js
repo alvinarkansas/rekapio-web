e350686b115a5baa98533e68f10d882d3736361d
alvinarkansas <alvinarkansas@gmail.com>
2022-01-15 06:07:21 +0700

    added vite-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,45 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "favicon-16x16.png",
+        "favicon-32x32.png",
+        "favicon.ico",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Rekapio",
+        short_name: "Rekapio",
+        description: "Track expenses easily with Rekapio. More features coming up very soon!",
+        theme_color: "#4F3C74",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: { port: 3001 },
   preview: { port: 3001 },
 });
