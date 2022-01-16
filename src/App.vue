d5eaffd270f3aa737475c33cb7bb4db093f83424
alvinarkansas <alvinarkansas@gmail.com>
2022-01-16 08:29:47 +0700

    new banner component, experimented install prompt

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
+  <BaseBanner @proceed="install" @dismiss="dismiss" v-model="deferredPrompt">
+    <p class="text-sm mb-2 text-neutral-100">
+      Get better experience with app instead of browser. It won't take up space
+      on your phone
+    </p>
+  </BaseBanner>
   <router-view />
 </template>
 
 <script>
+import BaseBanner from "./components/BaseBanner.vue";
+
 export default {
   name: "App",
+  components: {
+    BaseBanner,
+  },
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
   computed: {
     modal() {
       return this.$store.state.modal;
@@ -22,6 +38,27 @@ export default {
         }
       },
     },
+  },
+  methods: {
+    async dismiss() {
+      console.log("dismissed");
+      this.deferredPrompt = null;
+    },
+    async install() {
+      console.log("prompted");
+      this.deferredPrompt.prompt();
+    },
+  },
+  created() {
+    setTimeout(() => {
+      window.addEventListener("beforeinstallprompt", (e) => {
+        e.preventDefault();
+        this.deferredPrompt = e;
+      });
+      window.addEventListener("appinstalled", () => {
+        this.deferredPrompt = null;
+      });
+    }, 3000);
   },
 };
 </script>
---
+++ src/components/BaseBanner.vue
@@ -0,0 +1,56 @@
+<template>
+  <transition name="slide-down">
+    <div
+      class="fixed top-0 inset-x-0 bg-primary p-4 text-neutral-100"
+      v-if="modelValue"
+    >
+      <slot />
+
+      <div class="flex gap-2 items-center justify-end">
+        <BaseButton
+          label="Dismiss"
+          size="sm"
+          flavor="clear"
+          class="w-28"
+          @click="$emit('dismiss')"
+        />
+        <BaseButton
+          label="Install"
+          size="sm"
+          flavor="warning"
+          class="w-28"
+          @click="$emit('proceed')"
+        />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import BaseButton from "./BaseButton.vue";
+
+export default {
+  name: "BaseBanner",
+  components: {
+    BaseButton,
+  },
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+.slide-down-enter-from,
+.slide-down-leave-to {
+  opacity: 0;
+  transform: translateY(-100%) !important;
+}
+</style>
