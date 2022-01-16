0940f0074e03bb538118c8313d9160aafdc3caa5
alvinarkansas <alvinarkansas@gmail.com>
2022-01-16 08:39:31 +0700

    moved banner to main layout

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,4 +1,11 @@
 <template>
+  <BaseBanner @proceed="install" @dismiss="dismiss" v-model="deferredPrompt">
+    <p class="text-sm mb-2 text-neutral-100">
+      Get better experience with app instead of browser. It won't take up space
+      on your phone
+    </p>
+  </BaseBanner>
+
   <div class="bg-dark-300 text-neutral-100">
     <div class="py-4 mb-14">
       <router-view />
@@ -8,11 +15,38 @@
 </template>
 
 <script>
+import BaseBanner from "../components/BaseBanner.vue";
 import TheNavbar from "../components/TheNavbar.vue";
 
 export default {
   name: "Main",
-  components: { TheNavbar },
+  components: { TheNavbar, BaseBanner },
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
+  methods: {
+    async dismiss() {
+      console.log("dismissed");
+      this.deferredPrompt = null;
+    },
+    async install() {
+      console.log("prompted");
+      this.deferredPrompt.prompt();
+    },
+  },
+  created() {
+    setTimeout(() => {
+      window.addEventListener("beforeinstallprompt", (e) => {
+        e.preventDefault();
+        this.deferredPrompt = e;
+      });
+      window.addEventListener("appinstalled", () => {
+        this.deferredPrompt = null;
+      });
+    }, 3000);
+  },
   async mounted() {
     await this.$store.dispatch("loadCategories");
   },
